ee51830d96f741c6da369e17865429bc299bc1fc
kuldeepkarmaa <kuldeepkarma816@gmail.com>
2026-07-24 16:05:56 +0530

    Update authRoutes.js
    Change in Auth Routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
+// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, verifyRegisterOTP, loginUser, googleAuth} = require('../controllers/authController');
 
-// Mapping Endpoints
 router.post('/register', registerUser);
+router.post('/verify-otp', verifyRegisterOTP); // Make sure this route exists!
 router.post('/login', loginUser);
+router.post('/google', googleAuth);
 
 module.exports = router;
